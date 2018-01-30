a5883ea5f68d2735a61fa5ddd468b08233c1d410
TheAwesomePages <34585322+TheAwesomePages@users.noreply.github.com>
2018-01-30 18:45:52 -0500

    Changes
    Fixed mistakes in the code

--- login.js
+++ login.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-var allowedUsers =[ "bob" , "doodlemaster" , "bop34" , "ryan" , "eli"];
-var passwards =[ "1234" , "awesome" , "me34" , "2468" , "coding"];
-
-
-function myFunction(){
-	var user = document.getElementById("user").value;
-	var pass = document.getElementById("pass").value;
-	user = user.toLowerCase();
-
-	if(allowedUsers.indexOf(user)!= -1 && passwards.indexOf(pass)!= -1)  {
-		
-		if (passwards[allowedUsers.indexOf(user)] == pass) {
-			alert("You're in")
-			window.location.href="home.html";
-		}
-		
-	}
-
-	else
-	{
-		alert("Invalid Credentials");
-}
-
-
-}
-=======
 
 var allowedUsers =[ "bob" , "doodlemaster" , "bop34" , "ryan" , "eli"];
 var passwards =[ "1234" , "awesome" , "me34" , "2468" , "coding"];
@@ -52,4 +25,31 @@ function myFunction(){
 
 }
 
->>>>>>> 81111f8b455ec9993f2a339627bb46f1b3c67aa3
+
+var allowedUsers =[ "bob" , "doodlemaster" , "bop34" , "ryan" , "eli"];
+var passwards =[ "1234" , "awesome" , "me34" , "2468" , "coding"];
+
+
+function Function1(){
+	var user = document.getElementById("user").value;
+	var pass = document.getElementById("pass").value;
+	user = user.toLowerCase();
+
+	if(allowedUsers.indexOf(user)!= -1 && passwards.indexOf(pass)!= -1)  {
+		
+		if (passwards[allowedUsers.indexOf(user)] == pass) {
+			alert("You're in")
+			window.location.href="home.html";
+		}
+		
+	}
+
+	else
+	{
+		alert("Invalid Credentials");
+}
+
+
+}
+
+
