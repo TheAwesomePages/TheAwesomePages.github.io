f9ebc8d2aa7f02e811e8be04b98baa24debc64ea
TheAwesomePages <34585322+TheAwesomePages@users.noreply.github.com>
2018-02-03 18:46:05 -0500

    Changes2
    I added a calculater

--- math.js
+++ math.js
@@ -1,1 +1,7 @@
+function Sum() {
 console.log("hello");
+var multiply = document.getElementById("number").value;
+var multiply2 = document.getElementById("number2").value;
+var product = multiply2*multiply;
+alert(product);
+}
