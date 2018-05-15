990ba413f41607de95f00abff93e58e32ac9a4f5
TheAwesomePages <34585322+TheAwesomePages@users.noreply.github.com>
2018-05-15 19:06:24 -0400

    yuty
    we going to commit

--- math.js
+++ math.js
@@ -1,7 +1,13 @@
-function Sum() {
+function product1() {
 console.log("hello");
 var multiply = document.getElementById("number").value;
 var multiply2 = document.getElementById("number2").value;
 var product = multiply2*multiply;
 alert(product);
 }
+//multiply2.addEventListener("keyup", function(){
+// 	event.preventDefault();
+// 	if (event.keyCode === 13) {
+// 		document.getElementById("Multi").click();
+// 		}
+// 	})
